6347e26515a966fdcd599fe223cbbf7c1a249055
Kristen Bullock <kristenbullock84@gmail.com>
2018-01-16 04:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+  ]
+
+function destructivelyAppendKitten(array, element){
+  array.unshift('element');
+  return array;
+}
+
+function destructivelyPrependKitten(array, element){
+  
+}
+
+function destructivelyRemoveLastKitten(array, element){
+  
+}
+
+function destructivelyRemoveFirstKitten(array, element){
+  
+}
+
+function appendKitten(array, element){
+  
+}
+
+function prependKitten(array, element){
+  
+}
+
+function removeLastKitten(array, element){
+  
+}
+
+function removeFirstKitten(array, element){
+  
+}
+
